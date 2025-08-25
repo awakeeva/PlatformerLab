633968533b13e14e13aa5c3cd6b0e2f36786b3bf
AwakeEva <eva@akamo.info>
2025-08-26 01:53:42 +0300

    Stream 5 - Homework 4 - Create level that 3 screens size. + Display info about collected coins to the colsole

---
+++ Assets/PixelCrew/CoinTablo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace PixelCrew
+{
+    public class CoinTablo : MonoBehaviour
+    {
+        [SerializeField] private int SilverCoinCost;
+        [SerializeField] private int GoldCoinCost;
+
+        private int _silverCoinCount = 0;
+        private int _goldCoinCount = 0;
+
+        public void CollectSilverCoin()
+        {
+            _silverCoinCount++;
+            ShowTablo();
+        }
+        public void CollectGoldCoin()
+        {
+            _goldCoinCount++;
+            ShowTablo();
+        }
+
+        private void ShowTablo()
+        {
+            var total = _silverCoinCount * SilverCoinCost + _goldCoinCount * GoldCoinCost;
+            Debug.Log($"SilverCount ={_silverCoinCount} GoldCount ={_goldCoinCount} TotalMoney ={total}");
+        }
+    }
+}
