b2d59beec9c0595a3a808d0d54e7f6b1e8abc07f
AwakeEva <eva@akamo.info>
2025-08-27 00:51:36 +0300

    Stream 6 - Animation V1 - Unity Animator+Animation. Hero state: idle, run, jump, fall.

--- Assets/PixelCrew/Hero.cs
+++ Assets/PixelCrew/Hero.cs
@@ -11,10 +11,16 @@ public class Hero : MonoBehaviour
 
     private Rigidbody2D _rigidbody;
     private Vector2 _direction;
+    private Animator _animator;
+
+    private static readonly int isGroundKey = Animator.StringToHash("is-ground");
+    private static readonly int isRunningKey = Animator.StringToHash("is-running");
+    private static readonly int VerticalVelocityKey = Animator.StringToHash("vertical-velocity");
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     public void SetDirection(Vector2 direction)
@@ -27,10 +33,11 @@ public class Hero : MonoBehaviour
         _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
 
         var isJumping = _direction.y > 0;
+        var isGrounded = IsGrounded();
 
         if (isJumping)
         {
-            if (IsGrounded() && _rigidbody.velocity.y <= 0)
+            if (isGrounded && _rigidbody.velocity.y <= 0)
             {
                 _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
             }
@@ -39,6 +46,10 @@ public class Hero : MonoBehaviour
         {
             _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
         }
+
+        _animator.SetBool(isGroundKey, isGrounded);
+        _animator.SetFloat(VerticalVelocityKey, _rigidbody.velocity.y);
+        _animator.SetBool(isRunningKey, _direction.x != 0);
     }
 
     private bool IsGrounded()
