05fcaeeb54a87fe85767e62344ad4d3bea118484
AwakeEva <eva@akamo.info>
2025-09-17 20:10:11 +0300

    Coursework 1 - Six rooms + new feature (Movable Platform)

---
+++ Assets/PixelCrew/Components/ParentComponent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace PixelCrew.Components
+{
+    public class ParentComponent : MonoBehaviour
+    {
+        [SerializeField] private GameObject _parent;
+
+        public void SetParent(GameObject target)
+        {
+            target.transform.SetParent(_parent.transform);
+        }
+
+        public void ResetParent(GameObject target)
+        {
+            target.transform.SetParent(null);
+        }
+    }
+}
+
---
+++ Assets/PixelCrew/MovablePlatform.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+namespace PixelCrew
+{
+    public class MovablePlatform : MonoBehaviour
+    {
+        [SerializeField] private float _speedX;
+        [SerializeField] private float _speedY;
+
+        [SerializeField, Range(0, 15)] private float _xOffsetRight;
+        [SerializeField, Range(-15, 0)] private float _xOffsetLeft;
+
+        [SerializeField, Range(0, 15)] private float _yOffsetUp;
+        [SerializeField, Range(-15, 0)] private float _yOffsetDown;
+
+        private const float DirectionForward = 1.0f;
+        private const float DirectionBack = -1.0f;
+
+        private float _directionX = DirectionForward;
+        private float _directionY = DirectionForward;
+
+        private float _minX;
+        private float _maxX;
+        private float _minY;
+        private float _maxY;
+
+        private void Awake()
+        {
+            _minX = transform.position.x + _xOffsetLeft;
+            _maxX = transform.position.x + _xOffsetRight;
+
+            _minY = transform.position.y + _yOffsetDown;
+            _maxY = transform.position.y + _yOffsetUp;
+        }
+
+        private void Update()
+        {
+            var deltaXPosition = _directionX * _speedX * Time.deltaTime;
+            var newXPosition = transform.position.x + deltaXPosition;
+
+            var deltaYPosition = _directionY * _speedY * Time.deltaTime;
+            var newYPosition = transform.position.y + deltaYPosition;
+
+            transform.position = new Vector3(newXPosition, newYPosition, transform.position.z);
+            
+            _directionX = TryChangeDirection(_directionX, _minX, _maxX, transform.position.x);
+
+            _directionY = TryChangeDirection(_directionY, _minY, _maxY, transform.position.y);
+
+        }
+
+        private float TryChangeDirection(float direction, float minPos, float maxPos, float currentPos)
+        {
+            if (currentPos >= maxPos)
+            {
+                return DirectionBack;
+            }
+            else if (currentPos <= minPos)
+            {
+                return DirectionForward;
+            }
+
+            return direction;
+        }
+    }
+}
