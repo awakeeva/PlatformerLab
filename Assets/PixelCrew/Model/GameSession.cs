d4a3acd1b9872841087ec40a11dd7f93f85c1696
AwakeEva <eva@akamo.info>
2025-11-09 01:28:39 +0300

    Stream 10 - FIX - DestroyImmediate to Destroy

--- Assets/PixelCrew/Model/GameSession.cs
+++ Assets/PixelCrew/Model/GameSession.cs
@@ -12,7 +12,7 @@ namespace PixelCrew.Model
         {
             if (IsSessionExist())
             {
-                DestroyImmediate(gameObject);
+                Destroy(gameObject);
             }
             else
             {
