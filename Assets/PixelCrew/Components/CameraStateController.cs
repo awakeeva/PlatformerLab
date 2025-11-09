53285fed13c1c844754b8b6c8f11d687a7635f98
AwakeEva <eva@akamo.info>
2025-11-10 01:48:02 +0300

    Friday 5 - CutScene openning the door - Cinemachine State Driven Camera

---
+++ Assets/PixelCrew/Components/CameraStateController.cs
@@ -0,0 +1,25 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+namespace PixelCrew.Components
+{
+    public class CameraStateController : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+        [SerializeField] private CinemachineVirtualCamera _camera;
+
+        private static readonly int ShowTargetKey = Animator.StringToHash("ShowTarget");
+
+        public void SetPosition(Vector3 pos)
+        {
+            pos.z = _camera.transform.position.z;
+            _camera.transform.position = pos;
+        }
+
+        public void SetState(bool state)
+        {
+            _animator.SetBool(ShowTargetKey, state);
+        }
+    }
+}
+
