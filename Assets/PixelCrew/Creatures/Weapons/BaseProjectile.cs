541cb4911638f37e9cc2511de5e534491e9ff408
AwakeEva <eva@akamo.info>
2025-11-19 19:25:24 +0300

    Stream 13 - Seashell - Sinusoidal Projectile

---
+++ Assets/PixelCrew/Creatures/Weapons/BaseProjectile.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace PixelCrew.Creatures.Weapons
+{
+    public class BaseProjectile : MonoBehaviour
+    {
+        [SerializeField] protected float _speed;
+        [SerializeField] protected bool _invertX;
+
+        protected Rigidbody2D Rigidbody;
+        protected int Direction;
+
+        protected virtual void Start()
+        {
+            var mod = _invertX ? -1 : 1;
+            Direction = mod * transform.lossyScale.x > 0 ? 1 : -1;
+            Rigidbody = GetComponent<Rigidbody2D>();
+        }
+    }
+}
+
