89fd92bddf4598114e7f93c778a09aa70dd31685
AwakeEva <eva@akamo.info>
2025-11-24 13:09:55 +0300

    Stream 14 - the Door that requires a Key and Coins

---
+++ Assets/PixelCrew/Components/Interactions/RequireItemComponent.cs
@@ -0,0 +1,47 @@
+﻿using PixelCrew.Model;
+using PixelCrew.Model.Data;
+using PixelCrew.Model.Definitions;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PixelCrew.Components.Interactions
+{
+    public class RequireItemComponent : MonoBehaviour
+    {
+        [SerializeField] private InventoryItemData[] _required;
+        [SerializeField] private bool _removeAftreUse;
+
+        [SerializeField] private UnityEvent _onSuccess;
+        [SerializeField] private UnityEvent _onFail;
+
+        public void Check()
+        {
+            var session = FindObjectOfType<GameSession>();
+
+            var areAllRequirementsMet = true;
+
+            foreach (var item in _required)
+            {
+                var numItems = session.Data.Inventory.Count(item.Id);
+                if (numItems < item.Value)
+                    areAllRequirementsMet = false;
+            }
+
+            if (areAllRequirementsMet)
+            {
+                if (_removeAftreUse)
+                {
+                    foreach (var item in _required)
+                        session.Data.Inventory.Remove(item.Id, item.Value);
+                }
+                
+                _onSuccess?.Invoke();
+            }
+            else
+            {
+                _onFail?.Invoke();
+            }
+        }
+    }
+}
+
