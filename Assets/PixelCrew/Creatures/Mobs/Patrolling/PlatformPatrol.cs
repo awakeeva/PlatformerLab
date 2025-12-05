e52d03a257d9cef7deb916533ced0962f217f2f7
AwakeEva <eva@akamo.info>
2025-12-05 22:37:51 +0300

    Friday 7 - How to do homework 10 EXTRA - Platform Patrol

--- Assets/PixelCrew/Creatures/Mobs/Patrolling/PlatformPatrol.cs
+++ Assets/PixelCrew/Creatures/Mobs/Patrolling/PlatformPatrol.cs
@@ -1,13 +1,30 @@
 ﻿using System.Collections;
+using PixelCrew.Components.ColliderBased;
 using UnityEngine;
 
 namespace PixelCrew.Creatures.Mobs.Patrolling
 {
     public class PlatformPatrol : Patrol
     {
+        [SerializeField] private LayerCheck _groundCheck;
+        [SerializeField] private float _direction;
+        [SerializeField] private Creature _creature;
         public override IEnumerator DoPatrol()
         {
-            yield return null;
+            while (enabled)
+            {
+                if (_groundCheck.IsTouchingLayer)
+                {
+                    _creature.SetDirection(new Vector2(_direction, 0f));
+                }
+                else
+                {
+                    _direction = -_direction;
+                    _creature.SetDirection(new Vector2(_direction, 0f));
+                }
+
+                yield return null;
+            }
         }
     }
 }
