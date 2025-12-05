029eaa7eb81c2db7c9df385153c9e8f9c2418bea
AwakeEva <eva@akamo.info>
2025-12-06 00:06:42 +0300

    Friday 7 - Sharky - Obstacle check (Platform Patrol)

--- Assets/PixelCrew/Creatures/Mobs/Patrolling/PlatformPatrol.cs
+++ Assets/PixelCrew/Creatures/Mobs/Patrolling/PlatformPatrol.cs
@@ -7,13 +7,14 @@ namespace PixelCrew.Creatures.Mobs.Patrolling
     public class PlatformPatrol : Patrol
     {
         [SerializeField] private LayerCheck _groundCheck;
+        [SerializeField] private LayerCheck _obstacleCheck;
         [SerializeField] private float _direction;
         [SerializeField] private Creature _creature;
         public override IEnumerator DoPatrol()
         {
             while (enabled)
             {
-                if (_groundCheck.IsTouchingLayer)
+                if (_groundCheck.IsTouchingLayer && !_obstacleCheck.IsTouchingLayer)
                 {
                     _creature.SetDirection(new Vector2(_direction, 0f));
                 }
