3a0582298db575828cfb99088b221ca422b2a7e6
AwakeEva <eva@akamo.info>
2026-02-21 00:46:22 +0300

    Stream 18 - Audio settings - slider controller, property storage

--- Assets/PixelCrew/UI/Settings/SettingsWindow.cs
+++ Assets/PixelCrew/UI/Settings/SettingsWindow.cs
@@ -1,11 +1,21 @@
-﻿using PixelCrew.UI;
+﻿using PixelCrew.Model.Data;
+using PixelCrew.UI;
+using PixelCrew.UI.Widgets;
 using UnityEngine;
 
 namespace PixelCrew.UI.Settings
 {
     public class SettingsWindow : AnimatedWindow
     {
+        [SerializeField] private AudioSettingsWidget _music;
+        [SerializeField] private AudioSettingsWidget _sfx;
 
+        protected override void Start()
+        {
+            base.Start();
+            _music.SetModel(GameSettings.I.Music);
+            _sfx.SetModel(GameSettings.I.Sfx);
+        }
     }
 }
 
---
+++ Assets/PixelCrew/UI/Widgets/AudioSettingsWidget.cs
@@ -0,0 +1,46 @@
+﻿using PixelCrew.Model.Data.Properties;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PixelCrew.UI.Widgets
+{
+    public class AudioSettingsWidget : MonoBehaviour
+    {
+        [SerializeField] private Slider _slider;
+        [SerializeField] private Text _value;
+
+        private FloatPersistentProperty _model;
+
+        private void Start()
+        {
+            _slider.onValueChanged.AddListener(OnSliderValueChanged);
+        }
+
+        private void OnSliderValueChanged(float value)
+        {
+            _model.Value = value;
+        }
+
+        public void SetModel(FloatPersistentProperty model)
+        {
+            _model = model;
+            _model.OnChanged += OnValueChanged;
+            OnValueChanged(_model.Value, _model.Value);
+        }
+
+        private void OnValueChanged(float newValue, float oldValue)
+        {
+            var textValue = Mathf.Round(newValue * 100);
+            _value.text = textValue.ToString();
+            _slider.normalizedValue = newValue;
+        }
+
+        private void OnDestroy()
+        {
+            _model.OnChanged -= OnValueChanged;
+            _slider.onValueChanged.RemoveListener(OnSliderValueChanged);
+        }
+    }
+}
+
