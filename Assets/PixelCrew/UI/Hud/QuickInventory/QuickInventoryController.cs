06dc0926ea369876715705e85bb7811a05cddd88
AwakeEva <eva@akamo.info>
2026-02-26 18:23:27 +0300

    Stream 19 - Quick Inventory - textures and UI layout

---
+++ Assets/PixelCrew/UI/Hud/QuickInventory/QuickInventoryController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace PixelCrew.UI.Hud.QuickInventory
+{
+    public class QuickInventoryController : MonoBehaviour
+    {
+        [SerializeField] private Transform _container;
+        [SerializeField] private GameObject _prefab;
+
+        //private readonly CompositeDisposable _trash = new CompositeDisposable();
+
+    }
+}
+
