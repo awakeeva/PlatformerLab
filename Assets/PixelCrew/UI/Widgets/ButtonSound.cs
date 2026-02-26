d593e15b0c10f26539878c0ff05d88b25d268a9b
AwakeEva <eva@akamo.info>
2026-02-27 00:00:01 +0300

    Friday 9 - Refactoring Sfx coins - PlaySfxSound component

--- Assets/PixelCrew/UI/Widgets/ButtonSound.cs
+++ Assets/PixelCrew/UI/Widgets/ButtonSound.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using PixelCrew.Components.Audio;
+using PixelCrew.Utils;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace PixelCrew.UI.Widgets
@@ -11,7 +13,7 @@ namespace PixelCrew.UI.Widgets
         public void OnPointerClick(PointerEventData eventData)
         {
             if (_source == null)
-               _source = GameObject.FindWithTag("SfxAudioSource").GetComponent<AudioSource>();
+               _source = AudioUtils.FindSfxSource();
 
             _source.PlayOneShot(_audioClip);
         }
---
+++ Assets/PixelCrew/Utils/AudioUtils.cs
@@ -0,0 +1,15 @@
+﻿using PixelCrew.Components.Audio;
+using UnityEngine;
+
+namespace PixelCrew.Utils
+{
+    public class AudioUtils
+    {
+        public const string SfxSourceTag = "SfxAudioSource";
+        public static AudioSource FindSfxSource()
+        {
+            return GameObject.FindWithTag(SfxSourceTag).GetComponent<AudioSource>();
+        }
+    }
+}
+
