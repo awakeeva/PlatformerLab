06d4ea13ab9d52fce3a05acdcc4016f2ffb78342
AwakeEva <eva@akamo.info>
2026-03-02 16:01:18 +0300

    Stream 21 - Localization Manager, Localize Text component

---
+++ Assets/PixelCrew/Model/Definitions/Localization/LocalizationManager.cs
@@ -0,0 +1,42 @@
+﻿using PixelCrew.Model.Data.Properties;
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace PixelCrew.Model.Definitions.Localization
+{
+    public class LocalizationManager
+    {
+        public readonly static LocalizationManager I;
+
+        private StringPersistentProperty _localeKey = new StringPersistentProperty("en", "localization/current");
+        private Dictionary<string, string> _localization;
+
+        public event Action OnLocaleChanged;
+
+        static LocalizationManager()
+        {
+            I = new LocalizationManager();
+        }
+
+        public LocalizationManager()
+        {
+            LoadLocale(_localeKey.Value);
+        }
+
+        private void LoadLocale(string localeToLoad)
+        {
+            var def = Resources.Load<LocaleDef>($"Locales/{localeToLoad}");
+            _localization = def.GetData();
+            OnLocaleChanged?.Invoke();
+        }
+
+        public string Localize(string key)
+        {
+            return
+                _localization.TryGetValue(key, out var value) ? value : $"%%%{key}%%%";
+        }
+    }
+}
+
