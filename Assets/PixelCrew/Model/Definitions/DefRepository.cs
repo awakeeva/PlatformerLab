1975238971a38d459d3697f3143f702e6588cfdc
AwakeEva <eva@akamo.info>
2026-03-13 12:19:02 +0300

    Friday 11 - Create DefRepository<TDefType> , IHaveId

---
+++ Assets/PixelCrew/Model/Definitions/DefRepository.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace PixelCrew.Model.Definitions
+{
+    public class DefRepository<TDefType> : ScriptableObject where TDefType : IHaveId
+    {
+        [SerializeField] protected TDefType[] _collection;
+        public TDefType Get(string id)
+        {
+            foreach (var itemDef in _collection)
+            {
+                if (itemDef.Id == id)
+                    return itemDef;
+            }
+
+            return default;
+        }
+    }
+
+    public interface IHaveId
+    {
+        string Id { get; }
+    }
+}
+
