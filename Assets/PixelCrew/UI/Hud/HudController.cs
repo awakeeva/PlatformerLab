c588b1c519fcaab1c087f70977b518d394a8e93b
AwakeEva <eva@akamo.info>
2026-03-18 17:46:56 +0300

    Homework 21 - WIP - Perk in HUD controller

--- Assets/PixelCrew/UI/Hud/HudController.cs
+++ Assets/PixelCrew/UI/Hud/HudController.cs
@@ -1,6 +1,7 @@
 ﻿using PixelCrew.Model;
 using PixelCrew.Model.Definitions;
 using PixelCrew.UI.Widgets;
+using PixelCrew.UI.Windows.Perks;
 using PixelCrew.Utils;
 using System;
 using UnityEngine;
@@ -10,14 +11,33 @@ namespace PixelCrew.UI.Hud
     public class HudController : MonoBehaviour
     {
         [SerializeField] private ProgressBarWidget _healthBar;
+        [SerializeField] private PerkBar _perkBar;
 
         private GameSession _session;
 
         private void Start()
         {
             _session = FindObjectOfType<GameSession>();
+
             _session.Data.Hp.OnChanged += OnHealthChanged;
             OnHealthChanged(_session.Data.Hp.Value, 0);
+
+            _session.PerksModel.OnChanged += OnPerkChanged;
+            OnPerkChanged();
+        }
+
+        private void OnPerkChanged()
+        {
+            if (!string.IsNullOrEmpty(_session.PerksModel.Used))
+            {
+                _perkBar.gameObject.SetActive(true);
+                var def = DefsFacade.I.Perks.Get(_session.PerksModel.Used);
+                _perkBar.SetData(def, default);
+            }
+            else
+            {
+                _perkBar.gameObject.SetActive(false);
+            }
         }
 
         private void OnHealthChanged(int newValue, int oldValue)
@@ -35,6 +55,7 @@ namespace PixelCrew.UI.Hud
         private void OnDestroy()
         {
             _session.Data.Hp.OnChanged -= OnHealthChanged;
+            _session.PerksModel.OnChanged -= OnPerkChanged;
         }
     }
 }
---
+++ Assets/PixelCrew/UI/Windows/Perks/PerkBar.cs
@@ -0,0 +1,38 @@
+﻿using PixelCrew.Model;
+using PixelCrew.Model.Definitions;
+using PixelCrew.UI.Widgets;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PixelCrew.UI.Windows.Perks
+{
+    public class PerkBar : MonoBehaviour, IItemRenderer<PerkDef>
+    {
+        [SerializeField] private Image _icon;
+
+        private GameSession _session;
+        private PerkDef _data;
+
+        private void Start()
+        {
+            _session = FindObjectOfType<GameSession>();
+            UpdateView();
+        }
+
+        public void SetData(PerkDef data, int index)
+        {
+            _data = data;
+
+            if (_session != null)
+            {
+                UpdateView();
+            }
+        }
+
+        private void UpdateView()
+        {
+            _icon.sprite = _data.Icon;
+        }
+    }
+}
+
