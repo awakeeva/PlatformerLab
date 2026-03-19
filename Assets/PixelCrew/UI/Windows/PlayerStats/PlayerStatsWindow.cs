d4a13650522a36d8dae9910867d0d6d0958e4e6e
AwakeEva <eva@akamo.info>
2026-03-19 15:34:19 +0300

    Stream 24 - PlayerStatsWindow UI Layout - add Selector, add Icon for Stat

--- Assets/PixelCrew/UI/Windows/PlayerStats/PlayerStatsWindow.cs
+++ Assets/PixelCrew/UI/Windows/PlayerStats/PlayerStatsWindow.cs
@@ -1,9 +1,16 @@
-﻿using UnityEngine;
+﻿using PixelCrew.UI.Widgets;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace PixelCrew.UI.Windows.PlayerStats
 {
     public class PlayerStatsWindow : AnimatedWindow
     {
+        [SerializeField] private Transform _statsContainer;
+        [SerializeField] private StatWidget _prefab;
+
+        [SerializeField] private Button _buyButton;
+        [SerializeField] private ItemWidget _price;
     }
 }
 
---
+++ Assets/PixelCrew/UI/Windows/PlayerStats/StatWidget.cs
@@ -0,0 +1,15 @@
+﻿using PixelCrew.Model.Definitions;
+using PixelCrew.UI.Widgets;
+using UnityEngine;
+
+namespace PixelCrew.UI.Windows.PlayerStats
+{
+    public class StatWidget : MonoBehaviour, IItemRenderer<StatDef>
+    {
+        public void SetData(StatDef data, int index)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
+
