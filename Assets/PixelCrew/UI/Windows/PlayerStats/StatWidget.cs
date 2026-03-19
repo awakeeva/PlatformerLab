d4360fcd8563792ad0e146929e7c90ea3f29fb09
AwakeEva <eva@akamo.info>
2026-03-19 15:53:49 +0300

    Stream 24 - Add scripts to UI layout - StatWidget, PlayerStatsWindow. Set objects in serializable fields

--- Assets/PixelCrew/UI/Windows/PlayerStats/StatWidget.cs
+++ Assets/PixelCrew/UI/Windows/PlayerStats/StatWidget.cs
@@ -1,14 +1,21 @@
 ﻿using PixelCrew.Model.Definitions;
 using PixelCrew.UI.Widgets;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace PixelCrew.UI.Windows.PlayerStats
 {
     public class StatWidget : MonoBehaviour, IItemRenderer<StatDef>
     {
+        [SerializeField] private Image _icon;
+        [SerializeField] private Text _name;
+        [SerializeField] private Text _currentValue;
+        [SerializeField] private Text _increaseValue;
+        [SerializeField] private ProgressBarWidget _progress;
+        [SerializeField] private GameObject _selector;
+
         public void SetData(StatDef data, int index)
         {
-            throw new System.NotImplementedException();
         }
     }
 }
