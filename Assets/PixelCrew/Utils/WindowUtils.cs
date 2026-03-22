e460e60c6445412ccf297fc8448b336747699eea
AwakeEva <eva@akamo.info>
2026-03-22 21:33:17 +0300

    BugFix - InGameMenu on Level2, when Scene has several canvases

--- Assets/PixelCrew/Utils/WindowUtils.cs
+++ Assets/PixelCrew/Utils/WindowUtils.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 namespace PixelCrew.Utils
 {
@@ -7,7 +8,11 @@ namespace PixelCrew.Utils
         public static void CreateWindow(string resourcePath)
         {
             var window = Resources.Load<GameObject>(resourcePath);
-            var canvas = Object.FindObjectOfType<Canvas>();
+
+            //var canvas = Object.FindObjectOfType<Canvas>();
+            var canvases = Object.FindObjectsOfType<Canvas>();
+            var canvas = canvases.FirstOrDefault(x => x.renderMode == RenderMode.ScreenSpaceOverlay);
+
             Object.Instantiate(window, canvas.transform);
         }
     }
